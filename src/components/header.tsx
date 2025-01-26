5b8f19debfd42591d40639a8ce5c63d76aeafed3
Sax Dhiut <mail@iyxeyl.me>
2025-01-26 22:59:22 +0400

    feat: enhance header with icons and search input
    Added a search input field and icons to buttons for better UX. Adjusted layout for responsive behavior and updated styles for consistency.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,11 @@
 import { Logo } from "@/components/logo"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import { cn } from "@/lib/utils"
+import { Snowflake, Sun } from "lucide-react"
 
 export const HeaderLink = (props: { text: string }) => (
-	<Button variant={"link"} className={cn("text-muted-foreground")}>
+	<Button variant={"link"} className={cn("text-muted-foreground", "p-0")}>
 		{props.text}
 	</Button>
 )
@@ -22,14 +24,21 @@ export const Header = () => {
 			className={cn("flex", "flex-row", "justify-between", "p-4", "border-b")}
 		>
 			<Logo />
-			<div className={cn("flex", "flex-row")}>
+			<div className={cn(["hidden", "lg:flex"], "flex-row", "gap-x-4")}>
 				{headerLinks.map((link) => (
 					<HeaderLink key={link} text={link} />
 				))}
 			</div>
 			<div className={cn("flex", "flex-row", "gap-x-4")}>
-				<Button variant={"outline"}>Elit</Button>
-				<Button>Sapien</Button>
+				<Input placeholder={"Search..."} className={cn("w-44")} />
+				<Button variant={"outline"}>
+					<Sun />
+					Elit
+				</Button>
+				<Button>
+					<Snowflake />
+					Sapien
+				</Button>
 			</div>
 		</header>
 	)
