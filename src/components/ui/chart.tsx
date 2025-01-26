553920c06dd666d5cfb9162f4ce2d53b91d953f0
Sax Dhiut <mail@iyxeyl.me>
2025-01-26 19:34:20 +0400

    chore: add biome lint ignore comment for dangerouslySetInnerHTML

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -88,6 +88,7 @@ const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
 
 	return (
 		<style
+			// biome-ignore lint/security/noDangerouslySetInnerHtml: Yikes
 			dangerouslySetInnerHTML={{
 				__html: Object.entries(THEMES)
 					.map(
