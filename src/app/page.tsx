ec79796cf8b5c582622aac1a1e1acb1198d49137
Sax Dhiut <mail@iyxeyl.me>
2025-01-26 18:10:50 +0400

    feat: add max-w-screen-lg to main content grid for responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Page() {
 				{capitalize(`${faker.lorem.words(20)}.`)}
 			</h2>
 			<Button>{capitalize(`${faker.lorem.words(2)}`)}</Button>
-			<div className={cn("w-full", "grid", "grid-cols-12")}>
+			<div className={cn("w-full", "grid", "grid-cols-12", "max-w-screen-lg")}>
 				<div className={cn(["col-span-12", "md:col-span-7"], "w-full")}>
 					<CardSlider />
 				</div>
