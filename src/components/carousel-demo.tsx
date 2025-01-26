772b1ee817b422acdc0d7b57908c5851f7aa3eee
Sax Dhiut <mail@iyxeyl.me>
2025-01-26 19:34:33 +0400

    chore: add biome lint ignore comment for array index key

--- src/components/carousel-demo.tsx
+++ src/components/carousel-demo.tsx
@@ -24,6 +24,7 @@ export const CarouselDemo = () => (
 	>
 		<CarouselContent className="-ml-1">
 			{Array.from({ length: 5 }).map((_, index) => (
+				// biome-ignore lint/suspicious/noArrayIndexKey: Yikes #2
 				<CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
 					<div className="p-1">
 						<Card>
