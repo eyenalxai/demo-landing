0a823bc819e9c4738920d6c5f2e1cb7be4a8576d
Sax Dhiut <mail@iyxeyl.me>
2025-01-27 20:13:48 +0400

    feat: add RainbowButton component with animated background and color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,11 +57,17 @@ module.exports = {
 					3: "hsl(var(--chart-3))",
 					4: "hsl(var(--chart-4))",
 					5: "hsl(var(--chart-5))"
-				}
+				},
+				"color-1": "hsl(var(--color-1))",
+				"color-2": "hsl(var(--color-2))",
+				"color-3": "hsl(var(--color-3))",
+				"color-4": "hsl(var(--color-4))",
+				"color-5": "hsl(var(--color-5))"
 			},
 			animation: {
 				"border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
-				ripple: "ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite"
+				ripple: "ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite",
+				rainbow: "rainbow var(--speed, 2s) infinite linear"
 			},
 			keyframes: {
 				"border-beam": {
@@ -75,6 +81,14 @@ module.exports = {
 					},
 					"50%": {
 						transform: "translate(-50%, -50%) scale(0.9)"
+					}
+				},
+				rainbow: {
+					"0%": {
+						"background-position": "0%"
+					},
+					"100%": {
+						"background-position": "200%"
 					}
 				}
 			}
